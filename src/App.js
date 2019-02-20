4a7aa8f860dfc1f37f7744479d5b6c40a8991d12
wser <w.web.dev@gmail.com>
2019-02-20 08:58:44 +0100

    added axios

--- src/App.js
+++ src/App.js
@@ -1,60 +1,10 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 class App extends React.Component {
   render() {
-    return (
-      <div className="App">
-        Hello
-
-<div id="container" className="flexChild rowParent">
-  
-  <div id="rowChild2689" className="flexChild rowParent">
-    <div id="rowChild1419" className="flexChild columnParent">
-      <div id="columnChild82616" className="flexChild"></div>
-
-      <div id="columnChild71189" className="flexChild"></div>
-</div>
-
-    <div id="rowChild48272" className="flexChild columnParent">
-      <div id="columnChild76820" className="flexChild columnParent">
-        <div id="columnChild53139" className="flexChild columnParent">
-          <div id="columnChild7105" className="flexChild"></div>
-
-          <div id="columnChild16128" className="flexChild"></div>
-</div>
-
-        <div id="columnChild82567" className="flexChild columnParent">
-          <div id="columnChild83022" className="flexChild"></div>
-
-          <div id="columnChild29113" className="flexChild"></div>
-</div>
-</div>
-
-      <div id="columnChild27002" className="flexChild columnParent">
-        <div id="columnChild51104" className="flexChild columnParent">
-          <div id="columnChild23103" className="flexChild"></div>
-
-          <div id="columnChild86360" className="flexChild"></div>
-</div>
-
-        <div id="columnChild22189" className="flexChild columnParent">
-          <div id="columnChild49606" className="flexChild"></div>
-
-          <div id="columnChild97606" className="flexChild"></div>
-</div>
-</div>
-</div>
-</div>
-
-  <div id="rowChild93308" class="flexChild columnParent">
-    <div id="columnChild7326" class="flexChild"></div>
-
-    <div id="columnChild93841" class="flexChild"></div>
-</div>
-</div>
-
-      </div>
+    return ( <div className = "App" >
+      Hello </div>
     );
   }
 }
